a9a7ba779edcb155648c60aff208aa1895847dc2
DeveLoopzz <develoopzz00@gmail.com>
2024-12-09 10:27:25 +0100

    armor controller crud function defined, StoreArmorRequest validation data and error catching

---
+++ MHArmory/app/Http/Controllers/ArmorsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArmorsController extends Controller
+{
+    public function createArmor() {}
+
+    public function updateArmor() {}
+
+    public function deleteArmor() {}
+
+    public function readArmor() {}
+
+}
